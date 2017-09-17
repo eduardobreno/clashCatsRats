a1f86c3e26e13202869ab1f12499d9a0ccead63d
Eduardo Breno <eduardobreno@msn.com>
2017-09-17 00:04:35 -0400

    Initial Commit

---
+++ Assets/_Game/Scripts/AINavAgent.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AINavAgent : MonoBehaviour
+{
+	[Header ("GameObject with path to be followed")]
+	public GameObject waypoint;
+
+	private NavMeshAgent navAgent;
+	private List<Transform> waypoints = new List<Transform> ();
+	public float speed;
+	bool walkBack = false;
+	int wayPointIndex = 0;
+	private Animator anim;
+
+
+	Vector3 prevLoc = Vector3.zero;
+
+	void Start ()
+	{
+		navAgent = GetComponent<NavMeshAgent> ();
+		anim = GetComponentInChildren<Animator> ();
+
+		initWayPoints ();
+	}
+
+	void Update ()
+	{
+		List<Transform> target = GetComponent<FieldOfView> ().visibleTargets;
+		if (target.Count == 0) {
+			checkNextWayPoint ();
+		} else {
+			walkTo (target [0].transform.position);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "Player") {
+			print ("FIGHT");
+		}
+
+	}
+
+	void initWayPoints ()
+	{
+		Transform children = waypoint.transform;
+		for (int i = 0; i < children.childCount; i++) {
+			Transform child = children.GetChild (i).transform;
+			waypoints.Add (child);
+		}
+	}
+
+	void checkNextWayPoint ()
+	{
+		if (waypoints.Count > 1) {
+			if (Vector3.Distance (transform.position, waypoints [wayPointIndex].position) > 1) {
+				if (waypoints.Count - 1 == wayPointIndex) {
+					GetComponent<Animator> ().SetTrigger ("HandAttackRight");
+					GetComponent<Animator> ().SetBool ("walk", false);
+				} else {
+					walkTo (waypoints [wayPointIndex].position);
+				}
+			} else {
+				wayPointIndex = (walkBack) ? --wayPointIndex : ++wayPointIndex;
+				if (wayPointIndex == (waypoints.Count - 1))
+					walkBack = true;
+				if (wayPointIndex == 0)
+					walkBack = false;
+			}
+		}
+	}
+
+	void walkTo (Vector3 position)
+	{
+		navAgent.destination = position;
+		GetComponent<Animator> ().SetBool ("walk", true);
+	}
+
+	#if UNITY_EDITOR
+	void OnDrawGizmosSelected () {
+			// Draws a blue line from this transform to the target
+			Gizmos.color = Color.white;
+			Transform children = waypoint.transform;
+			for (int i = 0; i < children.childCount-1; i++) {
+				Gizmos.DrawLine (children.GetChild (i).transform.position, children.GetChild (i+1).transform.position);
+			}
+	}
+	#endif
+}
---
+++ Assets/_Game/Scripts/Spawn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Spawn : MonoBehaviour
+{
+	public enum SIDES {SideA,SideB}
+
+	public bool isAutomatic = true;
+	public GameObject spawnA;
+	public GameObject spawnB;
+	public SIDES player;
+	public SIDES computer;
+	public GameObject enemy;
+
+	void Start ()
+	{
+		if (isAutomatic) {
+			InvokeRepeating ("SpawnAutomatic", 3f, 3f);
+		}
+
+	}
+
+	public void SpawnObject (GameObject player)
+	{
+		Instantiate (player, spawnA.transform.position, spawnA.transform.rotation);
+	}
+
+	void SpawnAutomatic ()
+	{
+		GameObject spawnPoint;
+		if (computer == SIDES.SideB) {
+			spawnPoint = spawnB;
+		} else {
+			spawnPoint = spawnA;
+		}
+			
+		Instantiate (enemy, spawnPoint.transform.position, spawnPoint.transform.rotation);
+	}
+}
