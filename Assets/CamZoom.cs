8dca341a93862b4cc73073ea410226c3883cb7d6
Eduardo Breno <eduardobreno@msn.com>
2017-09-30 16:27:39 -0400

    add fixes

---
+++ Assets/CamZoom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamZoom : MonoBehaviour {
+
+	float minFov = 15f;
+	float maxFov = 90f;
+	private Camera cam;
+
+	void Start () {
+		cam = GetComponent<Camera> ();
+	}
+
+	public void ZoomIn(){
+		float fov =cam.fieldOfView;
+		fov -= 10;
+		fov = Mathf.Clamp(fov, minFov, maxFov);
+		cam.fieldOfView = fov;
+	}
+
+	public void ZoomOut(){
+		float fov =cam.fieldOfView;
+		fov += 10;
+		fov = Mathf.Clamp(fov, minFov, maxFov);
+		cam.fieldOfView = fov;
+	}
+}
